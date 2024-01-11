25a3db07fcf91aeb2c0200072e07c278f1491f0b
Abdul Rafay <152396110+abdulrafay-07@users.noreply.github.com>
2024-01-11 21:54:49 +0500

    Made the Amazon front page responsive.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function removeFirstOption() {
-    let firstOption = document.querySelector('#mySelect option:first-child');
-    firstOption.style.display = 'none';
-    document.getElementById('mySelect').removeEventListener('click', removeFirstOption);
+    let firstOption = document.querySelector("#mySelect option:first-child");
+    firstOption.style.display = "none";
+    document.getElementById("mySelect").removeEventListener("click", removeFirstOption);
 }
+
+function changePage() {
+    if (window.innerWidth <= 499 && !window.location.href.includes("mob.html")) {
+        window.location.href = "mob.html";
+    } else if (window.innerWidth > 499 && !window.location.href.includes("index.html")) {
+        window.location.href = "index.html";
+    }
+}
+
+// Call the function when the page loads and on window resize
+window.onload = changePage;
+window.onresize = changePage;
